26ceca0df82ab012751e33c2467a4190c0a6df60
JESSE <jessejzee@gmail.com>
2025-04-09 15:48:10 +0300

    Changes to agents onboarding

---
+++ ujuzi_stemex/ujuzi/src/api/agents-approval/controllers/agents-approval.ts
@@ -0,0 +1,7 @@
+/**
+ * agents-approval controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::agents-approval.agents-approval');
---
+++ ujuzi_stemex/ujuzi/src/api/agents-approval/routes/agents-approval.ts
@@ -0,0 +1,7 @@
+/**
+ * agents-approval router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::agents-approval.agents-approval');
